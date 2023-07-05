2f1a5e45819cfe5fefbff41911c86a0434b34285
Maria Vanessa Moreno <MariaVanessaMoreno@outlook.com>
2023-07-05 17:44:18 -0400

    Create Cards dynamically
    created consts, classes, and functions to add card elements and create them dynamically.

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 
 const cardObjectDefinitions = [
-    {id:1, imagePath:'images/card-KingHearts.png'},
-    {id:2, imagePath:'images/card-JackClubs.png'},
-    {id:3, imagePath:'images/card-QueenDiamonds.png'},
-    {id:4, imagePath:'images/card-AceSpades.png'}
+    {id:1, imagePath:'/images/card-KingHearts.png'},
+    {id:2, imagePath:'/images/card-JackClubs.png'},
+    {id:3, imagePath:'/images/card-QueenDiamonds.png'},
+    {id:4, imagePath:'/images/card-AceSpades.png'}
 ]
+
+const cardBackImgPath = '/images/card-back-Blue.png'
+
+const cardContainerElem = document.querySelector('.card-container')
+
 
 {/* <div class="card">
     <div class="card-inner">
@@ -43,12 +48,35 @@ function createCard(cardItem){
     addClassToElement(cardBackElem, 'card-back')
 
     //add src attribute and appropriate value to img element - back of card
-    addSrcToImageElem(cardBackElem, )
+    addSrcToImageElem(cardBackElem, cardBackImgPath)
+
+    //add src attribute and appropriate value to img element - front of card
+    addSrcToImageElem(cardFrontElem, cardItem.imagePath)
+
+    //assign class to back image element of back of card
+    addClassToElement(cardBackElem, 'card-img')
+
+    //assign class to front image element of front of card
+    addClassToElement(cardFrontElem, 'card-img')
+
+    //add front image element as child element to front card element
+    addChildElement(cardFrontElem, cardFrontImg)
+
+    //add back image element as child element to back card element
+    addChildElement(cardBackElem, cardBackImg)
+
+    //add front card element as child element to inner card element
+    addChildElement(cardInnerElem, cardFrontElem)
+
+    //add back card element as child element to inner card element
+    addChildElement(cardInnerElem, cardBackElem)
+
+    //add inner card element as child element to card element
+    addChildElement(cardElem, cardInnerElem)
 
 }
 function createElement(elemType){
     return document.createElement(elemType)
-
 }
 function addClassToElement(elem, className){
     elem.classList.add(className)
@@ -58,4 +86,7 @@ function addIdToElement(elem, id){
 }
 function addSrcToImageElem(imgElem, src){
     imgElem.src = src
+}
+function addChildElement(parentElem, childElem){
+    parentElem.appendChild(childElem)
 }
